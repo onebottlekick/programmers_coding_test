874e88ae85efd0d5ed5d801306995fa425a6e623
onebottlekick <gksqudcks97@gmail.com>
2021-07-17 15:42:40 +0900

    moved

--- inner_product.py
+++
@@ -1,7 +0,0 @@
-def solution(a, b):
-    answer_list = []
-    for i in  range(len(a)):
-            answer_list.append(a[i]*b[i])
-            
-    answer = sum(answer_list)
-    return answer
--- phoneketmon.py
+++
@@ -1,8 +0,0 @@
-def solution(nums):
-    selection=set(nums)
-    if (len(nums)/2)>=len(selection):
-        answer=len(selection)
-    else:
-        answer=len(nums)/2
-    
-    return answer
