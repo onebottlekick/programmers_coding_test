24e5c18a0a9716174246bda7958e74648dbf91c5
onebottlekick <gksqudcks97@gmail.com>
2022-01-15 11:13:03 +0900

    lotto

---
+++ level1/lotto.py
@@ -0,0 +1,6 @@
+def solution(lottos, win_nums):
+    inter = set(win_nums)&set(lottos)
+    sub = set(win_nums) - set(lottos)
+    min_rank = 7 - len(inter) if (7 - len(inter)) < 7 else 6
+    max_rank = min_rank - lottos.count(0) if (min_rank - lottos.count(0)) > 0 else 1
+    return [max_rank, min_rank]
